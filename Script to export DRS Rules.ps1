50b939093a29eb836980f2d1da3f83a3a87b99d4
algubellivenkatramreddy <33725321+algubellivenkatramreddy@users.noreply.github.com>
2020-03-04 22:49:02 -0600

    Add files via upload

---
+++ Script to export DRS Rules.ps1
@@ -0,0 +1,43 @@
+﻿
+#
+# PowerCLI Script to export DRS Rules
+# Created by BLiebowitz on 6/26/2017
+#
+#####################################################
+ 
+# Build an array for each vCenter to connect to
+$array = "vCenter1", "vCenter2", "vCenter3"
+for($count=0;$count -lt $array.length; $count++)
+{
+# Connect to vCenter
+connect-viserver $array[$count]
+ 
+# Set the export file name &amp; folder location.
+$export = "E:\vmware\DRS\$($array[$count])_DRSrules_$((Get-Date).ToString('MM-dd-yyyy')).txt"
+ 
+# Get the clusters to export rules from
+$clusters = get-cluster
+ 
+# Find and export rules for each cluster
+foreach ($cluster in $clusters) {
+ 
+# Find all the rules
+$rules = get-cluster -Name $cluster | Get-DrsRule
+ 
+# Export each rule
+foreach($rule in $rules){
+$line = (Get-View -Id $rule.ClusterId).Name
+$line += ("," + $rule.Name + "," + $rule.Enabled + "," + $rule.KeepTogether)
+ 
+foreach($vmId in $rule.VMIds){
+$line += ("," + (Get-View -Id $vmId).Name)
+}
+# Write each rule to a file.
+$line | add-content $export –force
+ 
+# Disconnect from vCenter
+disconnect-viserver $array[$count] –confirm:$false
+ 
+}
+}
+}
---
+++ scripr2_DRS rules splitted 2 scripts.ps1
@@ -0,0 +1,33 @@
+﻿The following script will save all rules to a .txt file.
+
+$outfile = "C:\rules.txt"
+Remove-Item $outfile
+$clusterName = <cluster-name>
+$rules = get-cluster -Name $clusterName | Get-DrsRule
+
+foreach($rule in $rules){
+  $line = (Get-View -Id $rule.ClusterId).Name
+  $line += ("," + $rule.Name + "," + $rule.Enabled + "," + $rule.KeepTogether)
+  foreach($vmId in $rule.VMIds){
+    $line += ("," + (Get-View -Id $vmId).Name)
+  }
+  $line | Out-File -Append $outfile 
+}
+The reason we write the rules to a .txt file is because the "Keep together" type of rules can have 2 or more guests defined.
+
+This type of info (variable length array) is impossible to export to a .CSV file.
+
+ 
+
+The 2nd script reads this external file and defines the rules.
+
+$file = "C:\rules.txt"
+$rules = Get-Content $file
+
+foreach($rule in $rules){
+  $ruleArr = $rule.Split(",")
+  if($ruleArr[2] -eq "True"){$rEnabled = $true} else {$rEnabled = $false}
+  if($ruleArr[3] -eq "True"){$rTogether = $true} else {$rTogether = $false}
+  get-cluster $ruleArr[0] | `
+    New-DrsRule -Name $ruleArr[1] -Enabled $rEnabled -KeepTogether $rTogether -VM (Get-VM -Name ($ruleArr[http://4..($ruleArr.Count - 1)|http://4..($ruleArr.Count - 1)])) 
+}
